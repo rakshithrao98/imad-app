d73b9986cb6f418e41cf6eee97aad73dcb8ec854
rakshithrao98 <rakshithrao98@users.noreply.github.com>
2018-02-17 21:56:31 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+var article-one={	
+	title:'Article One';
+	heading:'Heading 1';
+	date:'17/02/2018';
+	content:`<div class="container">
+	<p>This is the space for article one.
+	I am really enjoying this.</p>
+    </div>`;}
+
+function createTemplate(data){
+	var title=data.title;
+	var heading=data.heading;
+	var date=data.date;
+	var content=data.content;
+
+	var htmlTemplate=`<!DOCTYPE html>
+			<html>
+			<head>
+				<title>
+					${title}
+				</title>
+				<link rel="stylesheet" type="text/css" href="/styles.css">
+				<meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1"/>
+			</head>
+			<body>
+				<h1>${heading}</h1>
+				${content}
+			    <p>${date}</p>
+			</body>
+			</html>	`										
+												 
+	 return htmlTemplate;
+}
+
+
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/article-one',function(req,res){
+<<<<<<< HEAD
+   res.send(createTemplate(article-one)) ;
+=======
+   res.sendFile(path.join(__dirname,'ui','article-one.html')) ;
+>>>>>>> 88ef919030a306255a91ff4fd83a88d604ab14e2
+});
+
+articles={
+	article-one:{
+
+	}
+}
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
