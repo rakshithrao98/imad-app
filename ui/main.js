e3b27b824f527ff5ecbb802dbd102931f0482416
rakshithrao98 <rakshithrao98@users.noreply.github.com>
2018-02-21 23:38:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button= document.getElementById("counter");
+counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById("counter");
+    span.innerHTML=counter.toStinng();
+}
